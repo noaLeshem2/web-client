151def0d71ff77030ae40820f99a498bc1c3a17b
Inbal Apt <inbalapt@gmail.com>
2022-04-05 12:18:25 +0300

    s

--- app/src/LoginPage.js
+++ app/src/LoginPage.js
@@ -1,19 +1,22 @@
-
-function LoginPage(){
-    return(
+import RegisterPage from './RegisterPage';
+function LoginPage() {
+    return (
         <header className="App-header">
             <div class="container">
                 <div class="row justify-content-md-center">
-                        <ul class="fooo">
-                            <div>
-                                Username:<input id="username" placeholder="Enter Username"></input>
-                            </div>
-                            <div>
-                                Password:<input type="text" placeholder="Enter Password"></input>
-                            </div>
-                            <div><button type="button" class="btn btn-outline-primary">Login</button></div>
-                        </ul>
-                    </div>
+                    <ul class="fooo">
+                        <div>
+                            Username:<input id="username" placeholder="Enter Username"></input>
+                        </div>
+                        <div>
+                            Password:<input type="text" placeholder="Enter Password"></input>
+                        </div>
+                        <div>
+                            Not registered? <a href="RegisterPage.js" type="button" class="h">Click here</a> to register
+                        </div>
+                        <div><button type="button" class="btn btn-outline-primary">Login</button></div>
+                    </ul>
+                </div>
             </div>
         </header>
     );
