345d32303d3cc0276efff854d966e052797aa92d
Inbal Apt <inbalapt@gmail.com>
2022-04-05 11:57:08 +0300

    update

--- app/src/LoginPage.js
+++ app/src/LoginPage.js
@@ -1,24 +1,21 @@
+
 function LoginPage(){
     return(
         <header className="App-header">
-      <div class="container">
-              <div class="row justify-content-md-center">
-                <div class="col-md-auto">
-                  <ul class="fooo">
-                      <div>
-                          <span>Username:</span>
-                          <input type="text" placeholder="Enter Username"></input>
-                      </div>
-                      <div>
-                          <span>Password:</span>
-                          <input type="text" placeholder="Enter Password"></input>
-                      </div>
-                      <div><button type="button" class="btn btn-outline-primary">Login</button></div>
-                  </ul>
-                </div>
-              </div> 
-          </div>
-    </header>
+            <div class="container">
+                <div class="row justify-content-md-center">
+                        <ul class="fooo">
+                            <div>
+                                Username:<input id="username" placeholder="Enter Username"></input>
+                            </div>
+                            <div>
+                                Password:<input type="text" placeholder="Enter Password"></input>
+                            </div>
+                            <div><button type="button" class="btn btn-outline-primary">Login</button></div>
+                        </ul>
+                    </div>
+            </div>
+        </header>
     );
 }
 
