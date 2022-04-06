ef9763cc70712a3434b4982d53eaddaa649cfb67
noaLeshem2 <noaleshem99@gmail.com>
2022-04-06 11:45:52 +0300

    hui

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 import LoginPage from './LoginPage';
 import RegisterPage from './RegisterPage';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
 function App() {
   return (
     <div classname="Container-fluid">
       <div className="row">
-
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<LoginPage />}></Route>
-            <Route path='/Register' element={<RegisterPage />}></Route>
-          </Routes>
-        </BrowserRouter>
+      <RegisterPage></RegisterPage>
       </div>
     </div>
   );
---
+++ app/src/usersList.js
@@ -0,0 +1,9 @@
+
+const users = [{username : "inbal33", password : "1020", displayName : "Inbal"},
+               {username : "noale10", password : "454545", displayName : "Noa"},
+               {username : "yoval99", password : "09876", displayName : "Yoval"},
+               {username : "harel21", password : "0167", displayName : "Harel"}];
+
+
+               
+export default users;
