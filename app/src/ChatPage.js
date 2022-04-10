8fa56365a57ddad146d5419c122c65273aea5e56
Inbal Apt <inbalapt@gmail.com>
2022-04-10 20:16:16 +0300

    fbnh

--- app/src/ChatPage.js
+++ app/src/ChatPage.js
@@ -1,29 +1,31 @@
+import ChatingWith from './ChatingWith';
 import './ChatPage.css';
 import User from './usersFolder/User';
 import users from './usersFolder/usersList.js';
-
+import sami from './usersFolder/profile/sami.jpg';
 function ChatPage() {
     const userList = users.map((user, key) => {
         return <User {...user} key={key} />
     });
     return (
         <>
-           
-                <div className="row chating">
-                    <div className="col-3">
+            <div className="row chating">
+                <div className="col-3">
 
-                        <div className="chatList">
-                            {userList}
-                        </div>
+                    <div className="chatList">
+                        {userList}
+                    </div>
 
-                    </div>
-                    <div className="col-9">
-                        <div className="messages">
-                            <div>The CHAT</div>
-                        </div>
-                    </div>
                 </div>
-            
+                <div className="col-9">
+                        <div className="messages">
+                            <ChatingWith displayName={"shir"} time={"13:00"} message={"hi"} img={sami} />
+                        </div>
+
+                </div>
+            </div>
+
+
 
         </>
     );
