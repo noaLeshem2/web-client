da813b17c646f47cf16332321bbaf5370bdf4640
noaLeshem2 <noaleshem99@gmail.com>
2022-04-24 17:13:37 +0300

    gkg

--- app/src/Message.js
+++ app/src/Message.js
@@ -48,9 +48,31 @@ function Message({ type, text, time, mine }) {
         else if(type.localeCompare('audio') == 0) {
             return (<audio controls src={text}></audio>)
         }
+        //its video type
+        if(type.localeCompare('video') == 0){
+            console.log("videoooooooo")
+            return (
+                <>
+                    <div class="row message-body">
+                        <div class="col-sm-12">
+                            <div class="receiver">
+                                <div className="message-video">
+                                    <video controls src= {text}></video>
+                                </div>
+                                <span class="message-time pull-right">
+                                    {time}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            );
+        }
     }
+
     
     if (!mine) {
+        //this is a text type
         if (type.localeCompare('text') == 0) {
             return (
                 <>
@@ -88,9 +110,29 @@ function Message({ type, text, time, mine }) {
                 </>
             );
         } 
+        //its audio message
         else if(type.localeCompare('audio') == 0) {
             return (<audio controls src={text}></audio>)
         }
+        // its an image type
+        else if (type.localeCompare('video') == 0){
+            return (
+                <>
+                    <div class="row message-body">
+                        <div class="col-sm-12">
+                            <div class="sender">
+                                <div className="message-img">
+                                    <video controls src= {text}></video>
+                                </div>
+                                <span class="message-time pull-right">
+                                    {time}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            );
+        } 
     }
 
 }
